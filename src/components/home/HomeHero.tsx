feb62f2ab5906038ca8935abac084b9bbea84d05
tauhid23 <tauhidclover@gmail.com>
2026-04-28 11:02:27 +0600

    Search Section Fixed

--- src/components/home/HomeHero.tsx
+++ src/components/home/HomeHero.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import { motion, AnimatePresence, PanInfo } from "framer-motion";
 
 const images = [
-  "https://images.unsplash.com/photo-1483985988355-763728e1935b?auto=format&fit=crop&w=1600&q=80",
+  "https://i.ibb.co/tMmSFcZT/charlesdeluvio-FK81rxil-UXg-unsplash.jpg",
   "https://images.unsplash.com/photo-1523275335684-37898b6baf30?auto=format&fit=crop&w=1600&q=80",
   "https://images.unsplash.com/photo-1503602642458-232111445657?auto=format&fit=crop&w=1600&q=80",
 ];
